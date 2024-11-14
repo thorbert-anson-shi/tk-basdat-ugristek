49df151fac6368ea3155566ba21cb59a1f8ece2f
Mabacore101 <valentinozhuo2@gmail.com>
2024-11-14 15:14:55 +0700

    Membuat front end sederhana untuk halaman diskon.

--- diskon/views.py
+++ diskon/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.db import connection
 
+# Fungsi untuk tampilkan halaman diskon beserta data voucher dan promo.
 def show_hal_diskon(request):
-    return render(request, "hal_diskon.html")
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM diskon d JOIN voucher v ON d.Kode = v.Kode")
+        voucher = cursor.fetchall()
+        cursor.execute("SELECT * FROM promo")
+        promo = cursor.fetchall()
+
+    context = {
+        'data_voucher': voucher,
+        'data_promo': promo,
+    }
+
+    return render(request, "hal_diskon.html", context)
