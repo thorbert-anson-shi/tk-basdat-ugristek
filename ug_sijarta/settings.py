53f69b16ba2db1dc974ef7d85bdc073414097ad5
tobtoby <tobyas2005139@gmail.com>
2024-11-15 19:16:31 +0700

    Added Railway to list of allowed hosts

--- ug_sijarta/settings.py
+++ ug_sijarta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-cy6bffrh#g$jsuzw6ij_-^3plx4enhn*!o1@(64^znbt6*s=i#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["tk-basdat-ugristek-production.up.railway.app"]
 
 
 # Application definition
