0b7b24c734b29f1d4d1596789164e90013272ef5
tobtoby <tobyas2005139@gmail.com>
2024-11-15 17:59:01 +0700

    Finished MyPay frontend, and temporarily switched default db to a dummy db

---
+++ mypay/static/mypay/js/home.js
@@ -0,0 +1,80 @@
+const scrollContainer = document.querySelector("#scroll-container");
+const template = document.querySelector("#transaction-item");
+const transactionModal = document.querySelector("#transaction-modal");
+const transactionEndpoint = "/transactions/";
+
+function handleCreateTransaction() {
+  transactionModal.showModal();
+}
+
+function renderTransactions() {
+  // Empty table before each rerender
+  scrollContainer.replaceChildren();
+
+  const transactions = [
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+    { nominal: 200, tanggal: "2024-12-21", kategori: "good" },
+  ];
+
+  transactions.forEach((transaction) => {
+    let clone = template.content.cloneNode(true);
+
+    nominalP = clone.querySelector("#nominal");
+    tanggalP = clone.querySelector("#tanggal");
+    kategoriP = clone.querySelector("#kategori");
+
+    nominalP.innerHTML = transaction.nominal;
+    tanggalP.innerHTML = transaction.tanggal;
+    kategoriP.innerHTML = transaction.kategori;
+
+    scrollContainer.appendChild(clone);
+  });
+}
+
+async function fetchTransactions() {
+  let response = await fetch(transactionEndpoint);
+  let transactionList = response.json();
+  return transactionList;
+}
+
+window.onload = () => {
+  renderTransactions();
+};
