125224e2274bc393a8b3bb6a60fb790016e01334
tobtoby <tobyas2005139@gmail.com>
2024-11-17 23:40:47 +0700

    Finished pekerjaan-jasa panel

--- pekerjaan_jasa/static/pekerjaan_jasa/js/home.js
+++ pekerjaan_jasa/static/pekerjaan_jasa/js/home.js
@@ -1,6 +1,8 @@
 const kategoriDropdown = document.querySelector("#kategori-dropdown");
 const subkategoriDropdown = document.querySelector("#subkategori-dropdown");
 const optionTemplateRef = document.querySelector("#option-template");
+const resultsContainer = document.querySelector("#results");
+const resultTemplateRef = document.querySelector("#result-template");
 
 async function fetchCategories() {
   const response = await fetch("/pekerjaan_jasa/get_kategori");
@@ -18,9 +20,6 @@ async function fetchCategories() {
 }
 
 async function handleCategoryChange(category) {
-  // Let form input persist as long as window isn't closed
-  sessionStorage.setItem("kategori", category);
-
   const response = await fetch(
     "/pekerjaan_jasa/get_subkategori/?" +
       new URLSearchParams({ kategori: category })
@@ -40,11 +39,38 @@ async function handleCategoryChange(category) {
   });
 }
 
-async function handleSubcategoryChange(subcategory) {
-  const response = await fetch();
+async function handleSearch() {
+  const subcategory = subkategoriDropdown.value;
+  const response = await fetch(
+    "/pekerjaan_jasa/get_tickets/?" +
+      new URLSearchParams({ subkategori: subcategory })
+  );
+  const jsonData = await response.json();
+
+  resultsContainer.replaceChildren();
+
+  jsonData.data.forEach((ticket) => {
+    let clone = resultTemplateRef.content.cloneNode(true);
+
+    let subkategoriPesananEle = clone.querySelector("#subkategori-pesanan");
+    let namaPelangganEle = clone.querySelector("#nama-pelanggan");
+    let tanggalPemesananEle = clone.querySelector("#tanggal-pemesanan");
+    let tanggalPekerjaanEle = clone.querySelector("#tanggal-pekerjaan");
+    let biayaEle = clone.querySelector("#biaya");
+
+    subkategoriPesananEle.innerHTML = ticket.subkategori
+      .split("_")
+      .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
+      .join(" ");
+    namaPelangganEle.innerHTML = ticket.nama_pelanggan;
+    tanggalPemesananEle.innerHTML = ticket.tanggal_pemesanan;
+    tanggalPekerjaanEle.innerHTML = ticket.tanggal_pekerjaan;
+    biayaEle.innerHTML = ticket.biaya;
+
+    resultsContainer.appendChild(clone);
+  });
 }
 
 window.onload = () => {
-  sessionStorage.getItem("kategori");
   fetchCategories();
 };
--- pekerjaan_jasa/views.py
+++ pekerjaan_jasa/views.py
@@ -38,13 +38,52 @@ def get_tickets(request: HttpRequest):
 
     # Dummy data
     tickets_per_subcategory = {
-        "cuci_baju": [],
-        "cuci_sepatu": [],
-        "perbaikan_genteng": [],
-        "perbaikan_dinding": [],
+        "cuci_baju": [
+            {
+                "subkategori": "cuci_baju",
+                "nama_pelanggan": "danniel",
+                "tanggal_pemesanan": "2024-12-10",
+                "tanggal_pekerjaan": "2024-12-25",
+                "biaya": 20000,
+            },
+            {
+                "subkategori": "cuci_baju",
+                "nama_pelanggan": "sarah",
+                "tanggal_pemesanan": "2024-12-11",
+                "tanggal_pekerjaan": "2024-12-26",
+                "biaya": 25000,
+            },
+        ],
+        "cuci_sepatu": [
+            {
+                "subkategori": "cuci_sepatu",
+                "nama_pelanggan": "john",
+                "tanggal_pemesanan": "2024-12-12",
+                "tanggal_pekerjaan": "2024-12-27",
+                "biaya": 30000,
+            }
+        ],
+        "perbaikan_genteng": [
+            {
+                "subkategori": "perbaikan_genteng",
+                "nama_pelanggan": "mike",
+                "tanggal_pemesanan": "2024-12-13",
+                "tanggal_pekerjaan": "2024-12-28",
+                "biaya": 50000,
+            }
+        ],
+        "perbaikan_dinding": [
+            {
+                "subkategori": "perbaikan_dinding",
+                "nama_pelanggan": "anna",
+                "tanggal_pemesanan": "2024-12-14",
+                "tanggal_pekerjaan": "2024-12-29",
+                "biaya": 45000,
+            }
+        ],
     }
 
     tickets = tickets_per_subcategory[subcategory]
 
     jsonData = {"data": tickets}
-    return JsonResponse(tickets)
+    return JsonResponse(jsonData)
