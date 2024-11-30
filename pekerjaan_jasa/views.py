0889e54f7263294a60655f1baf9622e75cc5d84b
tobtoby <tobyas2005139@gmail.com>
2024-11-30 21:05:27 +0700

    Temporarily finished fetching categories and subcategories; Testing needed

--- pekerjaan_jasa/views.py
+++ pekerjaan_jasa/views.py
@@ -5,111 +5,14 @@ from django.views.decorators.csrf import csrf_exempt
 from enum import Enum
 import json
 
+# To make raw SQL queries
+from django.db import connection
+
 
 class Status(Enum):
     tiba = "Tiba Di Lokasi"
     in_progress = "Melakukan Pelayanan Jasa"
     selesai = "Selesai Melakukan Pelayanan"
-
-
-# Dummy data
-categories_to_subcategories = {
-    "perbaikan_rumah": ["Perbaikan Genteng", "Perbaikan Dinding"],
-    "mencuci": ["Cuci Baju", "Cuci Sepatu"],
-}
-
-tickets = [
-    {
-        "id": "756f4f52-9ef3-4667-a45a-8c7f5fbf7172",
-        "subkategori": "cuci_baju",
-        "nama_pelanggan": "danniel",
-        "tanggal_pemesanan": "2024-12-10",
-        "tanggal_pekerjaan": "2024-12-25",
-        "biaya": 20000,
-        "status": Status.tiba.value,
-    },
-    {
-        "id": "cb55e6f6-1928-4b4f-b964-8798bdae8f4d",
-        "subkategori": "cuci_baju",
-        "nama_pelanggan": "sarah",
-        "tanggal_pemesanan": "2024-12-11",
-        "tanggal_pekerjaan": "2024-12-26",
-        "biaya": 25000,
-        "status": Status.in_progress.value,
-    },
-    {
-        "id": "3a79befa-f462-4bc5-be59-de9fd2dec848",
-        "subkategori": "cuci_sepatu",
-        "nama_pelanggan": "john",
-        "tanggal_pemesanan": "2024-12-12",
-        "tanggal_pekerjaan": "2024-12-27",
-        "biaya": 30000,
-        "status": Status.selesai.value,
-    },
-    {
-        "id": "904c2ad9-1bd8-44ab-ba1a-6b5cf99ff33b",
-        "subkategori": "perbaikan_genteng",
-        "nama_pelanggan": "mike",
-        "tanggal_pemesanan": "2024-12-13",
-        "tanggal_pekerjaan": "2024-12-28",
-        "biaya": 50000,
-        "status": Status.tiba.value,
-    },
-    {
-        "id": "4cad1bdf-f9c0-416a-b113-4f8227a13e49",
-        "subkategori": "perbaikan_dinding",
-        "nama_pelanggan": "anna",
-        "tanggal_pemesanan": "2024-12-14",
-        "tanggal_pekerjaan": "2024-12-29",
-        "biaya": 45000,
-        "status": Status.in_progress.value,
-    },
-    {
-        "id": "ee669b1e-fff2-42b7-9f90-27cc2b430248",
-        "subkategori": "cuci_baju",
-        "nama_pelanggan": "alice",
-        "tanggal_pemesanan": "2024-12-15",
-        "tanggal_pekerjaan": "2024-12-30",
-        "biaya": 22000,
-        "status": Status.tiba.value,
-    },
-    {
-        "id": "950133e2-ed67-4581-950f-44890c6f39cf",
-        "subkategori": "cuci_sepatu",
-        "nama_pelanggan": "bob",
-        "tanggal_pemesanan": "2024-12-16",
-        "tanggal_pekerjaan": "2024-12-31",
-        "biaya": 32000,
-        "status": Status.in_progress.value,
-    },
-    {
-        "id": "9497e961-d290-46cd-b29d-5c41d33f48c3",
-        "subkategori": "perbaikan_genteng",
-        "nama_pelanggan": "charlie",
-        "tanggal_pemesanan": "2024-12-17",
-        "tanggal_pekerjaan": "2025-01-01",
-        "biaya": 55000,
-        "status": Status.selesai.value,
-    },
-    {
-        "id": "1cddcf9e-527a-46a6-823b-0d77a3c76f4d",
-        "subkategori": "perbaikan_dinding",
-        "nama_pelanggan": "david",
-        "tanggal_pemesanan": "2024-12-18",
-        "tanggal_pekerjaan": "2025-01-02",
-        "biaya": 47000,
-        "status": Status.tiba.value,
-    },
-    {
-        "id": "450d0ddd-9fbe-4693-b24a-239a235ad5fd",
-        "subkategori": "cuci_baju",
-        "nama_pelanggan": "eve",
-        "tanggal_pemesanan": "2024-12-19",
-        "tanggal_pekerjaan": "2025-01-03",
-        "biaya": 23000,
-        "status": Status.in_progress.value,
-    },
-]
 
 
 def home(request: HttpRequest):
@@ -121,20 +24,40 @@ def pekerjaan_list(request: HttpRequest):
 
 
 def get_categories(request: HttpRequest):
-    # Fetching categories from user
-    # ...
+    # TODO: Confirm correctness with auth service
+    user = request.session.get("user")
+    user_id = user["id"]
 
-    # Dummy data
-    categories = ["Perbaikan Rumah", "Mencuci"]
+    # Fetching categories from user
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "set search_path to sijarta;"
+            "select k.namakategori from kategori_jasa k "
+            "join pekerja_kategori_jasa p on k.id = p.kategorijasaid "
+            "where p.pekerjaid = %s",
+            [user_id],
+        )
+        categories = cursor.fetchall()
+        print(categories)
+
     jsonData = {"data": categories}
     return JsonResponse(jsonData)
 
 
 def get_subcategories(request: HttpRequest):
+    # TODO: Fetch category ID instead in order to keep things consistent
     category = request.GET.get("kategori")
+    category_id = category["id"]
 
     # Fetch subcategories from chosen category
-    subcategories = categories_to_subcategories[category]
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "select sj.namasubkategori from subkategori_jasa sj "
+            "join kategori_jasa k on k.id = sj.kategorijasaid "
+            "where sj.kategorijasaid = %s",
+            [category_id],
+        )
+        subcategories = cursor.fetchall()
 
     jsonData = {"data": subcategories}
     return JsonResponse(jsonData)
@@ -144,7 +67,17 @@ def get_tickets(request: HttpRequest):
     subcategory = request.GET.get("subkategori", default=None)
     status = request.GET.get("status", default=None)
 
-    filtered_tickets = copy.deepcopy(tickets)
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "select sj.namasubkategori as subkategori, p.nama, tpj.tglpemesanan, "
+            "tpj.tglpekerjaan, tpj.totalbiaya, st.statuspesanan as status "
+            "from tr_pemesanan_jasa tpj "
+            "join subkategori_jasa sj on sj.id = tpj.idkategorijasa "
+            "join users p on tpj.idpelanggan = p.id "
+            "join tr_pemesanan_status tps on tpj.id = tps.idtrpemesanan "
+            "join status_pesanan st on st.id = tps.idstatus"
+        )
+        filtered_tickets = dictfetchall(cursor)
 
     # Fetch tickets from chosen subcategory
     if subcategory != "" and subcategory is not None:
@@ -194,3 +127,8 @@ def get_status_choices(request: HttpRequest):
     return JsonResponse(
         {"choices": [(status.name, status.value) for status in (Status)]}
     )
+
+
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
